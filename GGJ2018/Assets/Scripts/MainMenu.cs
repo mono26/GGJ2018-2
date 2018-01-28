ea2531986432bc5735f96d34c15add00d2ee5dcb
Mono2692 <nzubiria1@gmail.com>
2018-01-28 00:54:52 -0500

    Main, buttons, and loads scene. TODO implement start when input.

---
+++ GGJ2018/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void PlayGame()
+    {
+        StartCoroutine(LoadGame());
+    }
+
+    public void Credits()
+    {
+        // TODO load credits screen
+    }
+
+    private IEnumerator LoadGame()
+    {
+        var load = SceneManager.LoadSceneAsync(1);
+        while(load.isDone)
+        {
+            // TODO loading screen
+        }
+        yield return null;
+    }
+}
